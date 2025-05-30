7a7dcd008d1814b4c3e389fbba7ebeacf32782e2
iphuck22 <eldeshue@naver.com>
2025-05-30 15:49:30 +0900

    initial commit : project setting

---
+++ tests/test_example.cc
@@ -0,0 +1,17 @@
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace shape {
+	namespace {
+
+		/*
+		// test example
+		TEST(ShapeTest, RectangleSize) {
+		  Rectangle rect(10, 2);
+		  EXPECT_EQ(rect.GetSize(), 20);
+		}
+		*/
+
+	}  // namespace
+}  // namespace shape
